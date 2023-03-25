d4e6dfb57272406cf8cacf7e85ce7ac96ef02f86
SirJohanot <pavelkrafft@gmail.com>
2023-03-25 17:37:07 +0300

    completed the task

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,14 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
+import java.util.Optional;
 
 public class Utils {
-    public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+    public static Optional<Boolean> isAllPositiveNumbers(List<String> args) {
+        return args.stream()
+                .map(StringUtils::isPositiveNumber)
+                .reduce((a, b) -> a && b);
     }
 }
