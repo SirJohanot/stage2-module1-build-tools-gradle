a8d1dc2cc3d7ec043ab5bde02be9c9c9cd70101a
SirJohanot <pavelkrafft@gmail.com>
2023-03-25 17:51:49 +0300

    changed return type of demo method

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -3,10 +3,9 @@ package com.epam.demo;
 import com.epam.utils.StringUtils;
 
 import java.util.List;
-import java.util.Optional;
 
 public class Utils {
-    public static Optional<Boolean> isAllPositiveNumbers(List<String> args) {
+    public static boolean isAllPositiveNumbers(List<String> args) {
         return args.stream()
                 .map(StringUtils::isPositiveNumber)
                 .reduce((a, b) -> a && b);
